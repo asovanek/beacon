42a7c4cd9a4e193ef2ab693cf6bdff6046f3032b
Igor Stojadinovic <igor.s@xola.com>
2016-12-03 20:47:42 +0530

    Refreshing every 30s

--- src/js/view/incoming_events.js
+++ src/js/view/incoming_events.js
@@ -2,16 +2,14 @@ import Marionette from "backbone.marionette";
 import EventSummaryView from "./event_summary";
 import moment from "moment";
 
+const THIRTY_SECONDS = 29999;
+
 export default Marionette.CollectionView.extend({
     childView: EventSummaryView,
 
-    initialize: function(options) {
-        this.filters = options.filters;
-
-        this.listenTo(this.collection, 'change', this.render);
-
+    fetchData: function () {
         var now = moment("2016-12-30");
-        
+
         this.collection.fetch({
             data: {
                 seller: this.filters.seller.get("id"),
@@ -20,5 +18,19 @@ export default Marionette.CollectionView.extend({
                 offset: new Date(now).getTimezoneOffset() * 60
             }
         });
+    },
+
+    refresh: function() {
+        var fetchData = this.fetchData.bind(this);
+        setInterval(fetchData, THIRTY_SECONDS);
+    },
+
+    initialize: function(options) {
+        this.filters = options.filters;
+
+        this.listenTo(this.collection, 'change', this.render);
+
+        this.fetchData();
+        this.refresh();
     }
 });
